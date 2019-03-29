bbe44dbb8db7d3f5ba44ccc1e34c214b277b9d86
johnbryce212 <1@1.com>
2019-03-29 09:46:30 +0300

    add Bank component

---
+++ Bank.js
@@ -0,0 +1,14 @@
+class Bank extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            withdrawAmount: 0,
+            depositAmount: 0,
+            balance: 1000
+        };
+    }
+
+    render() {
+        return e('div');
+    }
+}
